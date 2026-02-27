f8ea805535071eadd2e89e2f2c7510473204885e
Arkadiusz Bałakier <arkadiusz.balakier@gmail.com>
2026-02-27 14:12:02 +0100

    feat: add update method to UserSerializer, add url for "me" and token-auth, add LoginUserView and ManageUserView

--- user/serializers.py
+++ user/serializers.py
@@ -1,6 +1,5 @@
 from django.contrib.auth import get_user_model
-
-from cinema import serializers
+from rest_framework import serializers
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -12,3 +11,13 @@ class UserSerializer(serializers.ModelSerializer):
 
     def create(self, validated_data):
         return get_user_model().objects.create_user(**validated_data)
+
+    def update(self, instance, validated_data):
+        password = validated_data.pop("password")
+        user = super().update(instance, validated_data)
+
+        if password:
+            user.set_password(password)
+            user.save()
+
+        return user
--- user/urls.py
+++ user/urls.py
@@ -1,9 +1,12 @@
+from django.contrib.auth.views import LoginView
 from django.urls import path
 
-from user.views import CreateUserView
+from user.views import CreateUserView, LoginUserView, ManageUserView
 
 app_name = 'user'
 
 urlpatterns = [
     path("register/", CreateUserView.as_view(), name="create"),
+    path('token-auth/', LoginUserView.as_view(), name='token-auth'),
+    path("me/", ManageUserView.as_view(), name="manage"),
 ]
